87733e3d0263aeadb8522f4fa45672a133222844
jrichardsz <jrichardsz.java@gmail.com>
2020-04-12 11:46:45 -0500

    refactor and upgrade npm modules

--- server.js
+++ server.js
@@ -4,18 +4,14 @@ var app = express();
 // set the port of our application
 var port = process.env.PORT || 8080;
 
-// set the view engine to ejs
-app.set('view engine', 'ejs');
-
 // make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname)); // Current directory is root
+app.use(express.static(__dirname+"/web"));
 
 // set the home page route
 app.get('/', function(req, res) {
-    // ejs render automatically looks in the views folder
-    res.render('index');
+    res.sendFile('index.html');
 });
 
 app.listen(port, function() {
-    console.log('Our app is running on http://localhost:' + port);
+    console.log('Our app is running on ' + port);
 });
